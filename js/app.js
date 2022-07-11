2f9164d6542bfa741603c6ce077a736fc204c9e0
Crysource <crysource@Crysources-Air.lan>
2022-07-12 00:57:42 +0900

    learn to data type

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
-document.write("hello world");
 
-alert("susu boy");
+let name = "testName" , age = 35 , Name = "JavaScript" , NAME = "casesent";
+let $if = 10 , _for = "susu I kak"; // ตัวแปร คำสงวน
 
-console.log("variable")
+console.log(name);
+console.log(Name);
+console.log(NAME);
 
-console.error("virus")
+// const vat = 0.07;
+// console.log("Before = ", vat);
 
-console.warn("leaning")
+// vat = 0.08;
+// console.log("After = " , vat);
+
+
+console.log("Before = ", $if);
+
+$if = "change";
+console.log("After = " , $if);
+
+document.write(typeof(name));
+console.log("age =", age);
+
+let checkname = false;
+let checkproduct = true;
+let ayu = "20" + 10;
+let ayu2 = 20 + 10;
+
+
+console.log(checkname);
+console.log(checkproduct);
+console.log(typeof(checkproduct));
+console.log(ayu);
+console.log(ayu2);
+
+
+// document.write("hello world");
+
+// alert("susu boy");
+
+// console.log("variable")
+
+// console.error("virus")
+
+// console.warn("leaning")
+
+// let name;
+// name = "testName";
+// let age = 35;
